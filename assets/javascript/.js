f4701f4ce68053d2409e93eefb32c84fc0f455b1
whitelotus32 <87887692+whitelotus32@users.noreply.github.com>
2021-09-11 18:23:06 -0700

    Added function and response with fetch

--- assets/javascript/.js
+++ assets/javascript/.js
@@ -1,1 +1,29 @@
-test 
+var spiderman = function() {
+    console.log("function was called");
+};
+
+var response = fetch("API Function");
+console.log(response);
+
+
+/*
+var score = 0;
+
+var questions = [
+    {
+        title: "Were Peter Parker's parents regular citizens?",
+        choices: ["True", "False"],
+        answer: False
+    },
+    {
+        title: "Has Spider-Man ever been killed?",
+        choices: ["True", "False"],
+        answer: True
+    },
+    {
+        title: "Is Peter Parker the first Spider-Man?",
+        choices: ["True", "False"],
+        answer: False
+    },
+];
+*/ 
